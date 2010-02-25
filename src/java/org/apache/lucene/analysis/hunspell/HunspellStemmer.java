3e546fc90968780103f8b49cafa86710431b5bf5
gento0nz <gento0nz@2b531352-1995-11df-9007-2b600cd4dcf9>
2010-02-25 14:31:50 +0000

    Created basic skeleton for HunspellStemmer

---
+++ src/java/org/apache/lucene/analysis/hunspell/HunspellStemmer.java
@@ -0,0 +1,50 @@
+package org.apache.lucene.analysis.hunspell;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Chris Male
+ */
+public class HunspellStemmer {
+
+  private HunspellDictionary dictionary;
+
+  /**
+   * Constructs a new HunspellStemmer which will use the provided HunspellDictionary to create its stems
+   *
+   * @param dictionary HunspellDictionary that will be used to create the stems
+   */
+  public HunspellStemmer(HunspellDictionary dictionary) {
+    this.dictionary = dictionary;
+  }
+
+  /**
+   * Find the stem(s) of the provided word
+   * 
+   * @param word Word to find the stems for
+   *
+   * TODO (cmale) decide what this is going to return
+   */
+  public void stem(String word) {
+    
+  }
+
+  // ================================================= Helper Methods ================================================
+
+  
+}
