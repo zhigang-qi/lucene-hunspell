03819e4e410855b8d2c28415c13627e676e6c9f2
gento0nz <gento0nz@2b531352-1995-11df-9007-2b600cd4dcf9>
2010-02-25 17:10:46 +0000

    Added assertions to test

--- trunk/src/java/org/apache/lucene/analysis/hunspell/HunspellDictionary.java
+++ trunk/src/java/org/apache/lucene/analysis/hunspell/HunspellDictionary.java
@@ -17,20 +17,16 @@ package org.apache.lucene.analysis.hunspell;
  * limitations under the License.
  */
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import org.apache.lucene.analysis.CharArrayMap;
+import org.apache.lucene.util.Version;
+
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import org.apache.lucene.analysis.CharArrayMap;
-import org.apache.lucene.util.Version;
 
 public class HunspellDictionary {
 
