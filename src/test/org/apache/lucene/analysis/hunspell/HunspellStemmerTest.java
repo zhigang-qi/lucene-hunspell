235c3b71caab9a759c881994a14fccb8efa02ac3
gento0nz <gento0nz@2b531352-1995-11df-9007-2b600cd4dcf9>
2010-02-25 19:47:38 +0000

    Commented Stemmer Improved tests

--- src/test/org/apache/lucene/analysis/hunspell/HunspellStemmerTest.java
+++ src/test/org/apache/lucene/analysis/hunspell/HunspellStemmerTest.java
@@ -16,8 +16,14 @@ import static org.junit.Assert.assertEquals;
  */
 public class HunspellStemmerTest {
 
+  /**
+   * Pass condition: Word 'drinkable' should be stemmed to 'drink' with the suffix 'able' being stripped
+   * 
+   * @throws IOException Can be thrown while reading the files
+   * @throws ParseException Can be thrown while parsing the files
+   */
   @Test
-  public void testStem() throws IOException, ParseException {
+  public void testStem_simpleSuffixEnUS() throws IOException, ParseException {
     InputStream affixStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("dicts/en_US/en_US.aff");
     InputStream dictStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("dicts/en_US/en_US.dic");
 
@@ -29,12 +35,48 @@ public class HunspellStemmerTest {
     assertEquals(1, stems.size());
     assertEquals("drink", stems.get(0));
 
-    stems = stemmer.stem("remove");
+    affixStream.close();
+    dictStream.close();
+  }
+
+  /**
+   * Pass condition: Word 'remove' should be stemmed to 'move' with the prefix 're' being stripped
+   *
+   * @throws IOException Can be thrown while reading the files
+   * @throws ParseException Can be thrown while parsing the files
+   */
+  @Test
+  public void testStem_simplePrefixEnUS() throws IOException, ParseException {
+    InputStream affixStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("dicts/en_US/en_US.aff");
+    InputStream dictStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("dicts/en_US/en_US.dic");
+
+    HunspellDictionary dictionary = new HunspellDictionary(affixStream, dictStream);
+
+    HunspellStemmer stemmer = new HunspellStemmer(dictionary);
+    List<String> stems = stemmer.stem("remove");
 
     assertEquals(1, stems.size());
     assertEquals("move", stems.get(0));
+    
+    affixStream.close();
+    dictStream.close();
+  }
 
-    stems = stemmer.stem("drinkables");
+  /**
+   * Pass condition: Word 'drinkables' should be stemmed to 'drink' with the suffixes 's' and 'able' being removed recursively
+   *
+   * @throws IOException Can be thrown while reading the files
+   * @throws ParseException Can be thrown while parsing the files
+   */
+  @Test
+  public void testStem_recursiveSuffixEnUS() throws IOException, ParseException {
+    InputStream affixStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("dicts/en_US/en_US.aff");
+    InputStream dictStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("dicts/en_US/en_US.dic");
+
+    HunspellDictionary dictionary = new HunspellDictionary(affixStream, dictStream);
+
+    HunspellStemmer stemmer = new HunspellStemmer(dictionary);
+    List<String> stems = stemmer.stem("drinkables");
 
     assertEquals(1, stems.size());
     assertEquals("drink", stems.get(0));
@@ -42,4 +84,6 @@ public class HunspellStemmerTest {
     affixStream.close();
     dictStream.close();
   }
+
+
 }
